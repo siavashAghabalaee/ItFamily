b72dbf1fa24cdd23fa8b860109ca6e15814fc5a8
reza derikvand <reza.derikvand@gmail.com>
2020-03-05 12:07:04 +0330

    first comitt

--- app/src/main/java/com/zavosh/itfamily/fragments/ProfileFragment.kt
+++ app/src/main/java/com/zavosh/itfamily/fragments/ProfileFragment.kt
@@ -2,13 +2,16 @@ package com.zavosh.itfamily.fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.zavosh.itfamily.R
+import com.zavosh.itfamily.helper.Memory
+import com.zavosh.itfamily.myviews.MyToast
 import com.zavosh.itfamily.retrofit.Server
+import com.zavosh.itfamily.retrofit.mymodels.Callback
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,6 +19,10 @@ import com.zavosh.itfamily.retrofit.Server
 class ProfileFragment : Fragment() {
 
     private lateinit var fragmentView: View
+
+    private var sex: Boolean? = null
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,11 +34,34 @@ class ProfileFragment : Fragment() {
     }
 
     private fun setup() {
-        getProfileData()
+
+        phone_register.setText(Memory.loadPhone() ?: "")
+
+        if (name_register.text.isEmpty() || email_register.text.isEmpty()) {
+            MyToast.showToast(activity, "لطفا نام و ایمیل خود را وارد کنید")
+        } else {
+            if (chk_female.isChecked) {
+                sex = false
+            } else if (chk_male.isChecked) {
+                sex = true
+            }
+            getProfileData()
+        }
     }
 
     private fun getProfileData() {
-        Server.getInstance(context)
+
+
+        Server.getInstance(context).sendProfile(name_register.text.toString().trim(),
+            email_register.text.toString().trim(),
+            sex.toString(), loader,
+            object : Callback.PostProfile {
+                override fun callback(result: String?) {
+
+
+                }
+
+            })
     }
 
 
