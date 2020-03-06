7fced733069e673422aefdc251b3e736da5ac6ce
siavash aghabalaee <siavash.aghabalaee@gmail.com>
2020-03-06 18:50:53 +0330

    pod end

--- app/src/main/java/com/zavosh/itfamily/fragments/PodcastDetailFragment.kt
+++ app/src/main/java/com/zavosh/itfamily/fragments/PodcastDetailFragment.kt
@@ -6,9 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.jean.jcplayer.model.JcAudio
 import com.zavosh.itfamily.R
 import com.zavosh.itfamily.retrofit.mymodels.podcastlistrequest.PodcastListResult
 import kotlinx.android.synthetic.main.fragment_pod_detail.view.*
+import java.lang.Exception
+
 
 class PodcastDetailFragment: Fragment() {
 
@@ -43,21 +46,32 @@ class PodcastDetailFragment: Fragment() {
 
     private fun bindViews(podcastDetail: PodcastListResult?) {
 
-        rootView.tv_likes_count.text=podcastDetail?.linkeCount?:""
-        rootView.tv_comments.text=podcastDetail?.commentCount?:""
+        rootView.tv_likes_count.text=(podcastDetail?.linkeCount?:"")+" نفر پسندیده اند "
+        rootView.tv_comments.text= (podcastDetail?.commentCount?:"")+" نفر نظر داده اند "
         rootView.tv_title.text=podcastDetail?.title?:""
         rootView.tv_summery.text=podcastDetail?.summery?:""
         rootView.profile_avatar.setPicasso(podcastDetail?.image,activity)
-        rootView.link_address.text=podcastDetail?.linkAddress?:""
-        rootView.link_address2.text=podcastDetail?.linkAddress?:""
-        rootView.link_address3.text=podcastDetail?.linkAddress?:""
+        //rootView.link_address.text=podcastDetail?.linkAddress?:""
+        rootView.link_address2.text=podcastDetail?.publishDate?:""
+        //rootView.link_address3.text=podcastDetail?.linkAddress?:""
 
 
-        rootView.img_play.setOnClickListener {
+        /*rootView.img_play.setOnClickListener {
 
             Log.i("log","link : "+podcastDetail?.linkAddress)
 
-        }
+        }*/
+        val jcAudios: ArrayList<JcAudio> = ArrayList()
+        jcAudios.add(JcAudio.createFromURL("",podcastDetail?.linkAddress!!))
 
+        rootView.jcplayer.initPlaylist(jcAudios)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        rootView.jcplayer.kill()
+
+        //rootView.paus
     }
 }
