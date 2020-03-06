c6c9d03a9181ae8827c4f944b035a8a0fccf0d22
parisaalamshahi <parisa91>
2020-03-06 01:37:30 -0800

    add pari_article_details and parimagazin_details in XML

---
+++ app/src/main/java/com/zavosh/itfamily/MagazinDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.zavosh.itfamily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MagazinDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pari_articles_details);
+    }
+}
---
+++ app/src/main/java/com/zavosh/itfamily/ParimagazinDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.zavosh.itfamily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParimagazinDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parimagazin_details);
+    }
+}
